ba98cf48d6c7622e6f4d0f72f53e714e73d5c93c
Liam McNulty <Liam.McNulty@ibm.com>
2025-12-01 19:46:08 +0000

    day 01 - part 1 solution

--- 2025/01/solution.js
+++ 2025/01/solution.js
@@ -1,6 +1,6 @@
 const fs = require('node:fs');
 
-console.log('-> Retrieving Input Data');
+console.log('-> Retrieving Input Data - input.txt');
 let data;
 try {
     data = fs.readFileSync(__dirname + '/input.txt', 'utf8');
@@ -10,3 +10,55 @@ try {
     return;
 }
 
+const instructions = data.split('\n');
+
+console.log('-> Cracking Password');
+const solution = solve(instructions);
+
+console.log('');
+console.log('-> Password: ' + solution);
+
+
+function solve(instructions){
+
+    let position = 50;
+    let password = 0;
+
+    instructions.forEach(instruction => {
+
+        if (instruction === ''){ return; }
+
+        // Extract direction and clicks
+        const direction = instruction.substring(0, 1);
+        const clicks = parseInt(instruction.substring(1));
+
+        // Calculate new position
+        position = (direction === 'L') ? (position - clicks) : (position + clicks);
+
+        // Re-adjust Position
+        position = readjustTurns(position);
+
+        // Check if 0, therefore increase count for password
+        if (position === 0){
+            password++;
+        }
+
+    })
+
+    return password;
+}
+
+
+// Accounts for whole (and multiple) turns of the dial
+function readjustTurns(position){
+    while (position < 0 || position > 99){
+        if (position < 0){
+            position += 100;
+
+        } else if (position > 99){
+            position -= 100;
+        }
+    }
+
+    return position;
+}
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-# advent-of-code
+# Advent of Code
+## How to run
+Run with npm where -y argument is the year and -d is the day, e.g.:
+```
+npm run solve -- -y 2025 -d 01
+```
+
+## Solution
+- [2025](2025/)
+  - [Day 01 - Secret Entrance](2025/01/)
